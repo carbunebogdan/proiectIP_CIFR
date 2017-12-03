f15914f15b54e5d9aa0e649a52b0886bb94a05a3
Carbune Bogdan <carbunebogdan@gmail.com>
2017-12-03 12:59:00 +0200

    hello world

---
+++ CIFR/CIFR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CIFR
+//
+//  Created by Carbune Bogdan & Tiberiu Cocei on 03/12/2017.
+//  Copyright © 2017 Carbune Bogdan & Tiberiu Cocei. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"hello world";
+    return 0;
+}
